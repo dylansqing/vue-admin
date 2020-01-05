fbf6fb4ccb342a41db8a212b5c20124f53818548
dylansqing <dylansqing@outlook.com>
2020-01-05 22:19:50 +0800

    VUE/CLI4x项目开始

--- vue-admin/src/main.js
+++ vue-admin/src/main.js
@@ -1,23 +1,8 @@
-import Vue from "vue";
-import VueCompositionApi from "@vue/composition-api";
-import App from "./App.vue";
-import router from "./router/index.js";
-import store from "./store/index.js";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import "./router/premit";
-// 自定义全局方法
-// import global from "@/utils/global.js"; 
-// 自定义全局组件
-import "./icons";
-// Vue.use(global);
-Vue.use(ElementUI);
-Vue.use(VueCompositionApi);
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
 
-// runtime（运行模式）
+Vue.config.productionTip = false
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
